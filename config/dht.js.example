fe137d7e301364bc0ac630bf58f922ad86c2fbaf
Taylor Wong <cytaylorw@gmail.com>
2020-07-01 16:52:51 +0800

    Add wiston logger

--- config/dht.js.example
+++ config/dht.js.example
@@ -1,15 +1,19 @@
-module.exports = [
-    {
-        name: 'test',
-        type: 22,
-        pin: 1,
-        interval: 3000,
-        clientTime: true
-    },
-    {
-        name: 'test2',
-        type: 22,
-        pin: 1,
-        interval: 2000
-    },
-]
+module.exports = {
+    defaultInterval: 2000,
+    failureMultiplier: 2,
+    sensors: [
+        {
+            name: 'test',
+            type: 22,
+            pin: 1,
+            interval: 3000,
+            clientTime: true
+        },
+        {
+            name: 'test2',
+            type: 22,
+            pin: 1,
+            interval: 2000
+        },
+    ]
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const DhtSensor = require('./lib/DhtSensor');
-const dhtList = require('./config/dht');
+const dhtList = require('./config/dht').sensors;
 
 const mqtt = require('./lib/MqttClient');
+
+const loggers = require('./lib/Logger');
 
 
 
@@ -9,14 +11,16 @@ dhtList.forEach(dht => {
     dht.sensor = new DhtSensor(dht);
     dht.sensor.monitor();
     dht.sensor.on('data', data => {
-        console.log(data);
+        loggers.dht.debug('Get data: %s', data ,{label: 'onData'});
         if(mqtt.connected){
-            mqtt.publish('test',JSON.stringify(data))
-                // .then(() => {
-                //     console.log('published')
-                // })
-                .catch(err => {
-                    console.log(err);
+            let topic = `test/${data.name}`;
+            let message = JSON.stringify(data);
+            mqtt.publish(topic,message)
+                .then(() => {
+                    loggers.mqtt.debug('Published message: %s to topic: %s', message, topic, {label: 'publish'});
+                })
+                .catch(error => {
+                    loggers.mqtt.error(new Error(error),{label: 'publish'});
                 })
         }
     })
