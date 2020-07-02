67d16eebcad54149df7f5fb3b0338c5408525beb
Taylor Wong <cytaylorw@gmail.com>
2020-07-02 19:08:52 +0800

    Update config file implementation

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const DhtSensor = require('./lib/DhtSensor');
-const dhtList = require('./config/dht').sensors;
 
+const config = require('./lib/Config');
+const sensors = config.sensors;
 const mqtt = require('./lib/MqttClient');
 
 const loggers = require('./lib/Logger');
 
+console.log(config);
 
-
-dhtList.forEach(dht => {
-    dht.sensor = new DhtSensor(dht);
-    dht.sensor.monitor();
-    dht.sensor.on('data', data => {
-        loggers.dht.debug('Get data: %s', data ,{label: 'onData'});
-        if(mqtt.connected){
-            let topic = `test/${data.name}`;
-            let message = JSON.stringify(data);
-            mqtt.publish(topic,message)
-                .then(() => {
-                    loggers.mqtt.silly('Published message: %s to topic: %s', message, topic, {label: 'publish'});
-                })
-                .catch(error => {
-                    loggers.mqtt.error(new Error(error),{label: 'publish'});
-                })
-        }
-    })
+sensors.forEach(sensor => {
+    if(sensor.type === 'dht'){
+        sensor.instance = new DhtSensor(sensor.options);
+        sensor.instance.monitor();
+        sensor.instance.on('data', data => {
+            loggers.dht.debug('Get data: %s', data ,{label: 'onData'});
+            if(mqtt.connected){
+                let topic = `test/${sensor.name}`;
+                let message = JSON.stringify(data);
+                mqtt.publish(topic,message)
+                    .then(() => {
+                        loggers.mqtt.silly('Published message: %s to topic: %s', message, topic, {label: 'publish'});
+                    })
+                    .catch(error => {
+                        loggers.mqtt.error(new Error(error),{label: 'publish'});
+                    })
+            }
+        })
+    }
 })
