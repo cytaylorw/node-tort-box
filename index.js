6357e8d0a3c2519eadd141f6dad3e0f0fc2af7c0
Taylor Wong <cytaylorw@gmail.com>
2020-07-06 17:04:27 +0100

    Add TortBox class

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-const DhtSensor = require('./lib/DhtSensor');
-
-const config = require('./lib/Config');
-const sensors = config.sensors;
 const mqtt = require('./lib/MqttClient');
-
-const loggers = require('./lib/Logger');
-
-console.log(config);
-
-sensors.forEach(sensor => {
-    if(sensor.type === 'dht'){
-        sensor.instance = new DhtSensor(sensor.options);
-        sensor.instance.monitor();
-        sensor.instance.on('data', data => {
-            loggers.dht.debug('Get data: %s', data ,{label: 'onData'});
-            if(mqtt.connected){
-                let topic = `test/${sensor.name}`;
-                let message = JSON.stringify(data);
-                mqtt.publish(topic,message)
-                    .then(() => {
-                        loggers.mqtt.silly('Published message: %s to topic: %s', message, topic, {label: 'publish'});
-                    })
-                    .catch(error => {
-                        loggers.mqtt.error(new Error(error),{label: 'publish'});
-                    })
-            }
-        })
-    }
+const box = require('./lib/TortBox');
+box.init();
+mqtt.on('connect', connack => {
+    box.sendAttributes();
 })
+box.monitor();
