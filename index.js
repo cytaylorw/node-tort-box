4359db6d4e11c6d6a5183d8282f453bb0736b4d9
Taylor Wong <cytaylorw@gmail.com>
2020-07-07 15:25:14 +0800

    Add support for each sensor having its own MQTT client

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const mqtt = require('./lib/MqttClient');
 const box = require('./lib/TortBox');
-box.init();
-mqtt.on('connect', connack => {
-    box.sendAttributes();
-})
+
+box.init({sendAttributes: true});
 box.monitor();
